53281be7b225d70cfc56ec86b5f2658721ba1436
Avashneupane9857 <juisch20220011252@gmail.com>
2024-11-12 21:03:20 +0545

    :tada: init

---
+++ backend/src/index.ts
@@ -0,0 +1,5 @@
+/*
+
+build simple backend yesma req body aaucha code and langugage tei J0 ko API mah forward garney ani jun 
+response aaucha tei with status code send garney and or kei response aaye pani output dine ani teslai frontend bata output section mah fetch garcha 
+*/
